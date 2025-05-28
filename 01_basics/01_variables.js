4f1d1d1563aec7bbdbe418000ab57b454c954d05
Piyush Kumar Sharma <piyush.22scse1012889@galgotiasuniversity.edu.in>
2025-05-28 14:32:05 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = "piyushxrma222@google.com";
+var accountPassword = 243456;
+accountCity = "Delhi";
+let accountState;
+
+// accountId = 2;  (const can not be change)
+console.log(accountId);
+
+accountEmail = "piyush23@gmail.com";
+accountPassword = 22343532;
+console.log(accountEmail);
+console.log(accountPassword);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
