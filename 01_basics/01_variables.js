db4169c4f01c10cb08ffb628a76749958908eea1
Piyush Kumar Sharma <piyush.22scse1012889@galgotiasuniversity.edu.in>
2025-05-28 14:46:14 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -11,4 +11,8 @@ accountEmail = "piyush23@gmail.com";
 accountPassword = 22343532;
 console.log(accountEmail);
 console.log(accountPassword);
+
+/* Prefer not to use var
+because of issue in block scope and functional scope */
+
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
