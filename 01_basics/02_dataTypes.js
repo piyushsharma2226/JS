28264f9e63eebf9187ff55eb9cfd1935f3f3a0a9
Piyush Kumar Sharma <piyush.22scse1012889@galgotiasuniversity.edu.in>
2025-05-28 15:08:11 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3); // we are using nodejs, not browser (it will give error)
+
+let name = "Piyush";
+let age = 18;
+let isLoggedIn = false;
+
+/* PRIMITIVE DATATYPES */
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined => unique
+
+/* OBJECT */
+
+console.log(typeof name) // typeof used to know which type of datatype is.
+
+
